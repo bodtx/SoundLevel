ffcd65922043a46c79cdb88fd1f5ec2d939a0808
Alexis Krier <alexis.krier@laposte.net>
2014-12-11 22:05:02 +0100

    wake lock

--- src/org/bodtx/android/SoundLevelService.java
+++ src/org/bodtx/android/SoundLevelService.java
@@ -2,6 +2,7 @@ package org.bodtx.android;
 
 import java.io.IOException;
 import java.util.Calendar;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -18,38 +19,57 @@ import android.content.Intent;
 import android.media.AudioManager;
 import android.net.Uri;
 import android.os.IBinder;
-import android.support.v4.app.NotificationCompat;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 import android.util.Log;
 import android.widget.Toast;
 
 public class SoundLevelService extends Service {
 	ScheduledThreadPoolExecutor pool = new ScheduledThreadPoolExecutor(1);
 	boolean isAuBoulot = false;
+	private WakeLock wakeLock;
+	AudioManager audioManager ;
 
 	@Override
 	public void onCreate() {
-		NotificationCompat.Builder notification = new NotificationCompat.Builder(this)
-	    .setSmallIcon(R.drawable.ic_launcher)
-	    .setContentTitle("Gestion du mode");
+		audioManager=(AudioManager) this
+		.getSystemService(Context.AUDIO_SERVICE);
 		
-		startForeground(1, notification.build());
+		Notification.Builder notification = new Notification.Builder(this)
+				.setSmallIcon(R.drawable.ic_launcher).setContentTitle(
+						"Gestion du mode");
+
+		startForeground(1, notification.getNotification());
+		PowerManager mgr = (PowerManager) this
+				.getSystemService(Context.POWER_SERVICE);
+		wakeLock = mgr
+				.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MyWakeLock");
+		wakeLock.acquire();
 		pool.scheduleWithFixedDelay(new PeriodicTask(), 1, 30, TimeUnit.MINUTES);
 	}
 
 	private void setRingMode(int ringMode) {
-		AudioManager audioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+
 		audioManager.setRingerMode(ringMode);
 		if (ringMode == AudioManager.RINGER_MODE_NORMAL) {
-			// TODO pourquoi 1?
-			audioManager.setStreamVolume(AudioManager.STREAM_VOICE_CALL,
-					audioManager.getStreamMaxVolume(AudioManager.STREAM_VOICE_CALL), 1);
+			maxVolume(audioManager, AudioManager.STREAM_NOTIFICATION);
+			maxVolume(audioManager, AudioManager.STREAM_VOICE_CALL);
+			maxVolume(audioManager, AudioManager.STREAM_RING);
+			maxVolume(audioManager, AudioManager.STREAM_SYSTEM);
 		}
+	}
+
+	private void maxVolume(AudioManager audioManager, int type) {
+		// TODO pourquoi 1?
+		audioManager.setStreamVolume(type,
+				audioManager.getStreamMaxVolume(type), 1);
 	}
 
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
 		stopForeground(true);
+		wakeLock.release();
 		pool.shutdown();
 		Toast.makeText(this, "Service onDestroy() ", Toast.LENGTH_LONG).show();
 	}
@@ -58,22 +78,29 @@ public class SoundLevelService extends Service {
 
 		public void run() {
 			Log.i("PeriodicTimerService", "Awake");
-			
+
 			int jourSemaine = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
-			if (jourSemaine != Calendar.SATURDAY || jourSemaine != Calendar.SUNDAY) {
-				if (false/*connectToDevice("boulot")*/) {
+			if (/*
+				 * jourSemaine != Calendar.SATURDAY && jourSemaine !=
+				 * Calendar.SUNDAY
+				 */false) {
+				if (connectToDevice("00:15:83:3d:0a:57")) {
 					setRingMode(AudioManager.RINGER_MODE_VIBRATE);
 					isAuBoulot = true;
-				} else if (isAuBoulot && Calendar.getInstance().get(Calendar.HOUR_OF_DAY) > 16) {
-					Intent smsIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("sms:0687419862"));
-					smsIntent.putExtra("sms_body", "Je suis probablement déjà parti du boulot :-)");
+				} else if (isAuBoulot
+						&& Calendar.getInstance().get(Calendar.HOUR_OF_DAY) > 16) {
+					Intent smsIntent = new Intent(Intent.ACTION_SENDTO,
+							Uri.parse("sms:0687419862"));
+					smsIntent.putExtra("sms_body",
+							"Je suis probablement dï¿½jï¿½ parti du boulot :-)");
 					startActivity(smsIntent);
 					setRingMode(AudioManager.RINGER_MODE_NORMAL);
 					isAuBoulot = false;
 				}
 			}
-			
-			if (!isAuBoulot) {
+
+			if (!isAuBoulot
+					&& audioManager.getRingerMode() != AudioManager.RINGER_MODE_NORMAL) {
 				if (connectToDevice("20:13:10:15:38:91")) {
 					setRingMode(AudioManager.RINGER_MODE_NORMAL);
 				}
@@ -82,21 +109,32 @@ public class SoundLevelService extends Service {
 
 		private boolean connectToDevice(String macDevice) {
 			boolean success = false;
-			BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+			BluetoothAdapter mBluetoothAdapter = BluetoothAdapter
+					.getDefaultAdapter();
 
 			if (!mBluetoothAdapter.isEnabled()) {
 				mBluetoothAdapter.enable();
 			} else {
 
-				BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(macDevice);
+				// Set<BluetoothDevice> bondedDevices =
+				// mBluetoothAdapter.getBondedDevices();
+				// for (BluetoothDevice bondedDevice : bondedDevices) {
+				// if(bondedDevice.getName().equals("HC-05")){
+				// ;
+				// }
+				// }
+				BluetoothDevice device = mBluetoothAdapter
+						.getRemoteDevice(macDevice);
 				if (device == null) {
-					Log.i("connectToDevice", "Device " + macDevice + " non present\n");
+					Log.i("connectToDevice", "Device " + macDevice
+							+ " non present\n");
 				}
 				mBluetoothAdapter.cancelDiscovery();
 				BluetoothSocket socket = null;
 				try {
-					socket = device.createInsecureRfcommSocketToServiceRecord(UUID
-							.fromString("00001101-0000-1000-8000-00805F9B34FB"));
+					socket = device
+							.createInsecureRfcommSocketToServiceRecord(UUID
+									.fromString("00001101-0000-1000-8000-00805F9B34FB"));
 
 					socket.connect();
 					Thread.sleep(1000);
